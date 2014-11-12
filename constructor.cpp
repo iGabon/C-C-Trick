1aca6eb294fc0ec24d9546b75735916f02c6c7ef
gabon <igabon@163.com>
2014-11-12 10:46:34 +0800

    add construtror.cpp

---
+++ constructor.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class A
+{
+	private:
+			string name;
+			int age;
+			char gender;
+	public:
+			A(const string a,char gen):name(a),age(0),gender(gen){};
+			A(){};
+			void print();
+
+};
+
+void A::print()
+{
+		cout<<name<<endl;
+		cout<<age<<endl;
+		cout<<gender<<endl;
+}
+
+
+int main(void)
+{
+		A a("gabon",'c');
+		A b;
+		a.print();
+		cout<<"-----------------"<<endl;
+		b.print();
+		return 0;
+}
