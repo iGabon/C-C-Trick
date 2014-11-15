333c6a2e6549e4363f4829b6918affba98cb2b60
gabon <igabon@163.com>
2014-11-15 11:20:46 +0800

    修改了rand.cpp，本程序主要是用三种方法生成指定区间内的有序随机数

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 菜鸟的学习笔记：
 
 static.cpp 主要是用来测试static关键字的作用：在类定义体之内定义了static成员之后，必须在类的定义体之外再次进行定义，但是不需要再写上static关键字
+
 explicit.cpp 主要用来测试explicit关键字的作用：当类构造函数前面加了explicit关键字时，其他类型不能隐式转换为该类
+
 operator.cpp 主要用来测试operator关键字，程序中主要进行了输出操作符的重载
+
 construct.cpp 程序主要是用来测试构造函数的编写与使用
+
+rand.cpp 程序主要是解决编程珠玑第一章的练习题4,采用了三种方法生成1000000个介于0到10000000之间的有序的随机数
+
 /copy_construct/*   主要是用来测试复制构造函数的编写与使用
+
 /char shift/  其中的程序主要是字符串移位操作
+
            /left_shift.cpp 字符串左移操作
+
 		   /nonneighborBlock_exchange.cpp ABC转换成CBA,方法原理与STL中的rotate方法类似           
+
 		   /nonneighborBlock_exchange_improve.cpp ABC转换成CBA，方法为(A^B^C^)^=CBA,其中^为逆序符号           
+
--- rand.cpp
+++ rand.cpp
@@ -1,64 +1,93 @@
 //生成1000000个介于0到10000000之间的不同的随机数，并且用位图法排序
 #include<iostream>
 #include<ctime>
-#include<cstdlib>
 #include<algorithm>
-#include<bitset>
 #include<vector>
+#include<set>
 
 using namespace std;
 
-int main(void)
+/*
+ *生成m个介于0到n之间的有序的随机数
+ */
+void knuthRand(vector<int>& num,int m,int n)
 {
 		srand(time(0));
-		int i = 1000000;
-		vector<int> num;
-		
-		int m = 1000000;
-		for(int i = 0;i < 10000000;i++)//随机数生成算法，生成的数各不相同，编程珠玑120页
+		for(int i = 0;i<n;++i)
 		{
-			if((rand()%(10000000-i)) < m)
+			if((rand()%(n-i)) < m)
 			{
-				num.push_back(i);
-				m--;
+					num.push_back(i);
+					m--;
 			}
 		}
-		
-		vector<int> sortedNum;
-		time_t begin = time(0);
-		bitset<10000000> bit;
-		bit.reset();
-		int temp;	
-		for(vector<int>::iterator it = num.begin();it != num.end();++it)
-		{
-				temp = *it;
-				bit.set(temp);
-		}
-		for(int j = 0;j<10000000;j++)
-		{	
-			if(bit.test(j))
-					sortedNum.push_back(j);
-		}
-		time_t end = time(0);
-		cout<<"bitsort time : "<<difftime(end,begin)<<endl;
+}
 
-		begin = time(0);
+void setRand(set<int>& num,int m,int n)
+{
+	srand(time(0));
+	int temp;
+	while(m)
+	{
+			temp = rand();
+			temp = temp%n;
+			if(!num.count(temp))
+			{
+				num.insert(temp);
+				m--;
+			}
+
+	}
+}
+
+void swapRand(vector<int>& num,int m,int n)
+{
+		srand(time(0));	
+		int temp;
+		for(int i = 0;i<n;++i)
+		{
+				num.push_back(i);
+		}
+	int t;	
+		for(int j = 0;j < m;++j)
+		{
+				t = j+rand()%(n-j);
+				temp = num[j];
+				num[j] = num[t];
+				num[t] = temp;
+		}
 		sort(num.begin(),num.end());
-		end = time(0);
-		cout<<"quicksort time : "<<difftime(end,begin)<<endl;
+}
 
-/*
-		ofstream outfile_("sortedNum",ofstream::app);
-		if(outfile_)
+
+int main(void)
+{
+		int n = 100;
+		vector<int> num1;
+		set<int> num2;
+		vector<int> num3;
+		int m = 50;
+		knuthRand(num1,m,n);
+		setRand(num2,m,n);
+		swapRand(num3,m,n);
+
+		for(vector<int>::iterator it = num1.begin();it != num1.end();++it)
 		{
-				for(int j = 0;j<10000000;j++)
-				{
-						if(bit.test(j))
-								outfile_<<j<<endl;
-				}
+				cout<<*it<<' ';
 		}
-		outfile_.close();
-		outfile_.clear();
-		*/
+		cout<<endl;
+		
+		for(set<int>::iterator it = num2.begin();it != num2.end();++it)
+		{
+				cout<<*it<<' ';
+		}
+		cout<<endl;
+
+		for(vector<int>::iterator it = num3.begin();it < num3.begin()+m;++it)
+		{
+				cout<<*it<<' ';
+		}
+		cout<<endl;
+
 		return 0;
 }
